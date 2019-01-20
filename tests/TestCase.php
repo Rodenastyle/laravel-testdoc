aeca703da3651aebe35e34cc8267b385492ca8e6
sergio.rodenas <sergio.rodenas@sngular.team>
2019-01-20 15:15:11 +0100

    First commit

---
+++ tests/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergio.rodenas
+ * Date: 11/5/18
+ * Time: 20:45
+ */
+
+namespace Rodenastyle\TestDoc\Test;
+
+use PHPUnit\Framework\TestCase as BaseTest;
+
+abstract class TestCase extends BaseTest {}
